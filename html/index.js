9e5bf00002b7497de3a336beaf502f3bfb89c66f
Oscar Lajgaard <oscar@lajgaard-jensen.com>
2020-04-17 22:20:15 +0200

    Update index.js

--- html/index.js
+++ html/index.js
@@ -54,7 +54,7 @@ $(function () {
     $(".plate").on("keydown",function search(e) {
     	if(e.keyCode == 13) {
             var plate = $("#nplate").val();
-            if(document.getElementById("type").value == c) {plate = plate.slice(0, -2);}
+            if(document.getElementById("type").value == "c") {plate = plate.slice(0, -2);}
             
     		$.post('http://prp_policecentral/licenseCheck', JSON.stringify({
     			text: plate,
