228feaae83be316f46d07f50346a35dccecfe3ca
Oscar <oscar@lajgaard-jensen.com>
2020-04-20 10:37:18 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -26,6 +26,7 @@ function fetchLicense(user_id)
 
 	MySQL.Async.fetchAll("SELECT DmvTest FROM vrp_users WHERE id=@uid", {uid=user_id}, function(rows)
 		local license = rows[1].DmvTest
+		print(license)
 		if license == "3" then
 			print("ja")
 			return "Ja"
