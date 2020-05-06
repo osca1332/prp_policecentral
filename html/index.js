4f8072a775c81eebb1fe5b35ffbc3b86036ba31c
Oscar <oscar@lajgaard-jensen.com>
2020-05-06 10:24:46 +0200

    Ændret design

--- html/index.js
+++ html/index.js
@@ -1,9 +1,16 @@
+
+
+
 $(function () {
 
-    var name;
-    var firstname;
-    var phone;
-    var cpr;
+
+    var data = {
+        "Persons":[
+        {"firstName": "John","lastName": "Smith"},
+        {"firstName": "Lars","lastName": "Smith"},
+        ]
+    }
+
     function display(bool) {
         if (bool) {
             $(".tablet").show();
@@ -13,7 +20,7 @@ $(function () {
     }
 
 
-    display(false)
+    display(true)
 
     window.addEventListener('message', function(event) {
         var item = event.data;
@@ -24,26 +31,6 @@ $(function () {
                 display(false)
             }
         }
-
-        if (item.type === "id") {
-            document.getElementById("i-navn").value = item.first;
-            document.getElementById("i-efternavn").value = item.name;
-            document.getElementById("i-telf").value = item.phone;
-            document.getElementById("i-cpr").value = `${item.cpr}-${item.uid}`;
-            document.getElementById("i-alder").value = item.age;
-            document.getElementById("i-dmv").value = item.license;
-            $(".regi").html(item.record);
-        }
-
-        if (item.type === "idn") {
-            document.getElementById("i-navn").value = "Ikke registreret";
-            document.getElementById("i-efternavn").value = "Ikke registreret";
-            document.getElementById("i-telf").value = "00000000";
-            document.getElementById("i-cpr").value = "00000000-0";
-            document.getElementById("i-alder").value = "...";
-            document.getElementById("i-dmv").value = item.license;
-            $(".regi").html("...");
-        }
     })
     // if the person uses the escape key, it will exit the resource
     document.onkeyup = function (data) {
@@ -53,29 +40,25 @@ $(function () {
         }
     };
 
-    $(".plate").on("keydown",function search(e) {
+    function checkForValue(json, value) {
+        for (key in json) {
+            if (typeof (json[key]) === "object") {
+                return checkForValue(json[key], value);
+            } else if (json[key] === value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    $("#i-navn").on("keydown",function search(e) {
     	if(e.keyCode == 13) {
-            var plate = $("#nplate").val();
-            if(document.getElementById("type").value == "c") {
-                var p = plate.split("_");
-                $.post('http://prp_policecentral/nameCheck', JSON.stringify({
-                    first: p[0],
-                    last: p[1]
-                }));
-                return
-            }
-
-            if(document.getElementById("type").value == "t") {
-                $.post('http://prp_policecentral/phoneCheck', JSON.stringify({
-                    phone: plate,
-                }));
-                return
-            }
-
-    		$.post('http://prp_policecentral/licenseCheck', JSON.stringify({
-    			text: plate,
-    		}));
-            return
+            let input = document.getElementById("i-navn").value;
+            /* 
+                Her skal vi sende request til databasen om en liste med alle navne der passer på input.
+            
+            */
+            return;
 	    }
 	});
 
@@ -83,6 +66,5 @@ $(function () {
         $.post('http://prp_policecentral/exit', JSON.stringify({}));
         return
     });
-
 
 })
