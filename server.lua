cc9962d3cf1e1550dab7b98c74d8f8feed4b9aa5
Oscar <oscar@lajgaard-jensen.com>
2020-05-07 09:18:42 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -27,15 +27,17 @@ RegisterNetEvent("p_check")
 AddEventHandler("p_check", function(data)
 	local pl = source
 	MySQL.Async.fetchAll("SELECT * FROM vrp_user_identities WHERE @usid in (firstname , age ,registration , name, phone)", {usid = data}, function(rows)
-		local temp = {
+		local person = {
     	}
 		if #rows > 0 then
 			for i = 1, #rows do
-				temp[i] = rows[i]
+				person[i] = rows[i]
+				person[i].rec = MySQL.Sync.fetchScalar("SELECT dvalue FROM vrp_user_data WHERE dkey='vRP:police_records' AND user_id=@uid", {uid=person[i].user_id})
 			end
 		end
+
 		
-		TriggerClientEvent("prp:returnQuery", pl, temp)
+		TriggerClientEvent("prp:returnQuery", pl, person)
 	end)
 end)
 
